e3767267f3ee437892d7247b53d789ffc7c2f25f
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2023-01-15 13:24:31 +0530

    enhance: delete image validator updated

--- src/functions/image-request-upload-url-s3/schema.ts
+++ src/functions/image-request-upload-url-s3/schema.ts
@@ -1,7 +1,19 @@
 import * as Joi from "joi";
 
+export function filenameWithExtensionValidator(value: string): string {
+  const split = value.split(".");
+  if (split.length != 2) {
+    throw new Error("Invalid filename");
+  }
+  if (split[0] === "" || split[1] === "") {
+    throw new Error("Invalid filename");
+  }
+
+  return value;
+}
+
 export const imageRequestPresignedURLReqQuerySchema = Joi.object({
-  filename: Joi.string().required(),
+  filename: Joi.string().custom(filenameWithExtensionValidator).required(),
 });
 
 export interface imageRequestPresignedURLReqQuerySchemaType {
